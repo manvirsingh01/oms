6deb004e21b7e8964b77c4ed1445b81e2ac8c8c7
Manvir singh <manvirbalap498@gmail.com>
2024-12-25 11:06:30 +0530

    outpass amagment syste

---
+++ js/student.js
@@ -0,0 +1,16 @@
+function toggleForm() {
+    var form = document.getElementById("outpassForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+function toggleForm(formId) {
+    var form = document.getElementById(formId);
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
